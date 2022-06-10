611767058af947a9bd49f39f546d3ae824cf2642
Brígida Oliveira <ocfb0914@hotmail.com>
2022-06-10 17:14:31 -0300

    Update questao02_v01.kt

--- Nivel_01/Algoritmos/questao_02/questao02_v01.kt
+++ Nivel_01/Algoritmos/questao_02/questao02_v01.kt
@@ -1,3 +1,13 @@
+//Faça um programa que calcule a média de três notas e exibe o resultado no console.
+
 fun main() {
-    println()
+    println("\nPROGRAMA QUE CALCULA A MÉDIA DE 3 NOTAS")
+    
+    val n1 = 10.0
+    val n2 = 8.5
+    val n3 = 6.3
+
+    val media = n1 + n2 + n3
+
+    println("A média das notas $n1, $n2 e $n3 é: $media.")
 }
