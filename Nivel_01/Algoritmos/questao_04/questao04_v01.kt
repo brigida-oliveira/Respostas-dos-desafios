38d7ae1a9dde7137b5f722d0f5f8b26f9719d525
Brígida Oliveira <ocfb0914@hotmail.com>
2022-06-10 18:03:28 -0300

    Update and rename questao01_v01.kt to questao04_v01.kt

---
+++ Nivel_01/Algoritmos/questao_04/questao04_v01.kt
@@ -0,0 +1,12 @@
+//Faça um programa que calcula a área de um triângulo. Sabe-se que: Área = (base * altura) / 2
+
+fun main() {
+    println("\nPROGRAMA QUE CALCULA A ÁREA DE UM TRIÂNGULO\n")
+
+    val base = 4.0
+    val altura = 5.0
+
+    val area = (base*altura)/2
+
+    println("A área de um triângulo de base $base e altura $altura é $area.")
+}
