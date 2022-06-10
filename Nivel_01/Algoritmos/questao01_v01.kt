73b6a50b22143738c8256e0271263434cbfa06e0
Brígida <ocfb0914@hotmail.com>
2022-06-10 09:28:56 -0300

    respostas nível 1

---
+++ Nivel_01/Algoritmos/questao01_v01.kt
@@ -0,0 +1,11 @@
+//1. Faça um programa que soma quatro número inteiros e exibe o resultado da soma no console.
+fun main(){
+    val n1 = 5
+    val n2 = 2
+    val n3 = 9
+    val n4 = 3
+
+    val resposta = n1 + n2 + n3 + n4
+
+    println("A soma de $n1, $n2, $n3 e $n4 é igual a $resposta.")
+}
