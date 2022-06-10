e02e11333fb6519fcb995f9ddf025a95f30fb367
Brígida Oliveira <ocfb0914@hotmail.com>
2022-06-10 10:26:24 -0300

    Create questao01_v02.kt

---
+++ Nivel_01/Algoritmos/questao01_v02.kt
@@ -0,0 +1,19 @@
+//1. Faça um programa que soma quatro número inteiros e exibe o resultado da soma no console.
+fun main(){
+  println("\nPROGRAMA QUE SOMA 4 NÚMEROS\n")
+  println("Digite o primeiro número: ")
+  val n1 = readLine()!!.toInt()
+  
+  print("Digite o segundo número: ")
+  val n2 = readLine()!!.toInt()
+  
+  print("Digite o terceiro número: ")
+  val n3 = readLine()!!.toInt()
+  
+  print("Digite o quarto número: ")
+  val n4 = readLine()!!.toInt()
+
+  val resposta = n1 + n2 + n3 + n4
+
+  println("\nA soma de $n1, $n2, $n3 e $n4 é igual a $resposta.")
+}
