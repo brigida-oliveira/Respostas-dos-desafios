5b666bebceb534dfd57687cfc529f3b2b9df9957
Brígida Oliveira <ocfb0914@hotmail.com>
2022-06-10 18:23:15 -0300

    Update questao01_v01.kt

--- Nivel_01/Algoritmos/questao_05/questao01_v01.kt
+++ Nivel_01/Algoritmos/questao_05/questao01_v01.kt
@@ -1,3 +1,26 @@
+/*5. Faça um programa que calcula:
+    1. o número ao quadrado;
+    2. o número ao cubo;
+    3. a raiz quadrada;
+    4. a raiz cúbica;*/
+
+import kotlin.math.sqrt
+import kotlin.math.pow
+
 fun main() {
-    println()
+    println("\nPROGRAMA QUE ELEVA UM NÚMERO AO QUADRADO E DEPOIS AO CUBO E LOGO EM SEGUIDA FAZ A RAIZ QUADRADA E A RAIZ CÚBICA DESSE NÚMERO\n")
+
+    val numero = 64.0
+
+    val quadrado = numero*numero
+    val cubo = numero*numero*numero
+    val raizQuadrada = sqrt(numero)
+    val raizCubica = numero.pow(1.0 / 3.0) 
+
+    println("""
+    Número $numero elevado ao quadrado: $quadrado
+    Número $numero elevado ao cubo: $cubo
+    Raiz quadrada de $numero: $raizQuadrada
+    Raiz cúbica de $numero: $raizCubica
+    """)
 }
