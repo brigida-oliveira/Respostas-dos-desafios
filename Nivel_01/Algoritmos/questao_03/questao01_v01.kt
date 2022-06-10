dfa6434a5be0037fb1825b187605f1661b0d7738
Brígida Oliveira <ocfb0914@hotmail.com>
2022-06-10 17:43:16 -0300

    Update questao01_v01.kt

--- Nivel_01/Algoritmos/questao_03/questao01_v01.kt
+++ Nivel_01/Algoritmos/questao_03/questao01_v01.kt
@@ -1,3 +1,10 @@
+//Faça um programa que calcula e mostra o salário de um funcionário que teve um aumento de 25%.
+
 fun main() {
-    println()
+    println("\nPROGRAMA QUE CALCULA O SALÁRIO DE UM FUNCIONÁRIO QUE TEVE AUMENTO DE 25%\n")
+    val salarioAntigo = 1593.45
+    println("Salário sem aumento: $salarioAntigo")
+    val aumento = salarioAntigo*25/100
+    val salarioAtual = salarioAntigo + aumento
+    println("Salário sem aumento: $salarioAtual")
 }
