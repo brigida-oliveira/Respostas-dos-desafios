95bf42ba4877348765266bbe12db34b4bbe96457
Brígida Oliveira <ocfb0914@hotmail.com>
2022-06-10 23:09:14 -0300

    Update questao01_v02.kt

--- Nivel_01/Estrutura_condicional/questao_04/questao01_v02.kt
+++ Nivel_01/Estrutura_condicional/questao_04/questao01_v02.kt
@@ -1,3 +1,12 @@
+//4. Faça um programa que recebe um número inteiro e verifica se o número é ímpar ou par.
 fun main() {
-    println()
+    println("\nPROGRAMA QUE VERIFICA SE UM NÚMERO É PAR OU ÍMPAR\n")
+
+    print("Digite um número: ")
+    val numero = readLine()!!.toInt()
+
+    if (numero % 2 == 0)
+        println("\nO número $numero é par.")
+    else
+        println("\nO número $numero é ímpar.")
 }
