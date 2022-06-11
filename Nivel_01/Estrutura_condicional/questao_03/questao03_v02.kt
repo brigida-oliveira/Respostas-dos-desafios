03a0f2fb9fe5209f599c900dfaac3a7ae894f046
Brígida Oliveira <ocfb0914@hotmail.com>
2022-06-10 23:31:41 -0300

    Update questao03_v02.kt

--- Nivel_01/Estrutura_condicional/questao_03/questao03_v02.kt
+++ Nivel_01/Estrutura_condicional/questao_03/questao03_v02.kt
@@ -2,27 +2,44 @@
 fun main() {
     println("\nPROGRAMA QUE ORDENA 3 NÚMEROS\n")
 
-    val numeros = arrayOf(0.0, 0.0, 0.0)
-
     print("Digite o primeiro número: ")
-    numeros[0] = readLine()!!.toDouble()
+    val n1 = readLine()!!.toInt()
 
     print("Digite o segundo número: ")
-    numeros[1] = readLine()!!.toDouble()
+    val n2 = readLine()!!.toInt()
 
     print("Digite o terceiro número: ")
-    numeros[2] = readLine()!!.toDouble()
+    val n3 = readLine()!!.toInt()
 
-    println("\nNúmeros escolhidos: ${numeros[0]}, ${numeros[1]}, ${numeros[2]}")
+    println("\nNúmeros escolhidos: $n1, $n2, $n3")
 
-    
-for(posicaoAtual in 0 until (numeros.size -1)) {
-    if( numeros[posicaoAtual] > numeros[posicaoAtual+1]) {
-        val temp = numeros[posicaoAtual]
-        numeros[posicaoAtual] = numeros[posicaoAtual+1]
-        numeros[posicaoAtual+1] = temp
+    val numeros = arrayOf(0, 0, 0)
+
+    if (n1 < n2 && n1 < n3 && n2 < n3){
+        numeros[0] = n1
+        numeros[1] = n2
+        numeros[2] = n3
+    } else if (n1 < n2 && n1 < n3 && n3 < n2){
+        numeros[0] = n1
+        numeros[1] = n3
+        numeros[2] = n2
+    } else if (n2 < n1 && n2 < n3 && n1 < n3){
+        numeros[0] = n2
+        numeros[1] = n1
+        numeros[2] = n3
+    } else if (n2 < n1 && n2 < n3 && n3 < n1){
+        numeros[0] = n2
+        numeros[1] = n3
+        numeros[2] = n1
+    } else if (n3 < n1 && n3 < n2 && n1 < n2){
+        numeros[0] = n3
+        numeros[1] = n1
+        numeros[2] = n2
+    } else if (n3 < n1 && n3 < n2 && n2 < n1){
+        numeros[0] = n3
+        numeros[1] = n2
+        numeros[2] = n1
     }
-}
 
 println("Números ordenados: ${numeros[0]}, ${numeros[1]}, ${numeros[2]}")
 }
