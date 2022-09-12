7c749dd3cdda8c47db0c869b52a374b832e8439e
Brígida Oliveira <ocfb0914@hotmail.com>
2022-09-12 08:34:34 -0300

    Mudanças
    Acréscimo de explicação em algumas respostas

--- Nivel_01/Arrays/questao_02/questao01_v02.kt
+++ Nivel_01/Arrays/questao_02/questao01_v02.kt
@@ -1,3 +1,5 @@
+//Faça um programa que tem um vetor com nove números inteiros, 
+//calcule e mostre os números primos e suas respectivas posições.
 fun main() {
     println()
 }
--- Nivel_01/Arrays/questao_03/questao01_v02.kt
+++ Nivel_01/Arrays/questao_03/questao01_v02.kt
@@ -1,3 +1,5 @@
+//Faça um programa que preencha um vetor com 10 números inteiros aleatórios entre 1 e 12,
+//calcule e mostre o vetor resultado de uma ordenação decrescente.
 fun main() {
     println()
 }
